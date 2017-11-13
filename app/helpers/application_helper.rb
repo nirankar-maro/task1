6ba14213aa9a96bc0601987ea4a10381ac5a4f75
nirankar-maro <nirankar@maropost.com>
2017-11-13 13:41:13 +0530

    modify footer link and without login link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active?(path)
     "active" if current_page?(path)
   end
+
+  def login?
+    if user_signed_in?
+      return true
+    else
+      return false
+    end
+  end
 end
