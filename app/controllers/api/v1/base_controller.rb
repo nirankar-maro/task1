28ad12675790a0c493131ebf52ee06b9ee4b9c98
nirankar-maro <nirankar@maropost.com>
2017-11-30 00:35:13 +0530

    Implementad authenticate_user method for authenticate

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,24 @@
 class Api::V1::BaseController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_user
   respond_to :json
+  private
+  def authenticate_user
+    email = params[:email]
+    password = params[:password]
+    @user = User.find_by(email: email)
+    if @user.present?
+      if password != nil
+        if @user.valid_password?(password)
+        else
+          render json: {Err_msg: "Email or Password is Invalid",status: "Failure"}
+        end
+      else
+        render json: {Err_msg: "Password is Invalid",status: "Failure"}
+      end
+    else
+      render json: {Err_msg: "Email is Invalid",status: "Failure"}
+    end
+  end
 
 end
--- app/controllers/api/v1/contact_details_controller.rb
+++ app/controllers/api/v1/contact_details_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::ContactDetailsController < Api::V1::BaseController
+  skip_before_action :authenticate_user
   respond_to :json
   def create
     @contact_detail = ContactDetail.new(contact_us_params)
     if @contact_detail.save
       @email = ContactEmail.new
       Thread. new{
-      @email.thankyou_mail(@contact_detail)  
+      @email.thankyou_mail(@contact_detail)
       }
-      render json: {message: "Successful" ,status: "Success"}
+      render json: {Message: "Successful" ,status: "Success"}
 
     else
       render json: {Err_msg: @contact_detail.errors.full_messages,status: "Failure"}
